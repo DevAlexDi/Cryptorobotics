a3ca4dc6392740871bff38c9b99d0ed8ae47e3cc
lexa <keitmark@mail.ru>
2018-02-21 11:46:34 +0300

    qq

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
     $('.plus').click(function(){
-        
+        if($(this).parents('.flex-form-soc').hasClass('opened')){
+            $(this).parents('.flex-form-soc').removeClass('opened');
+        }
+        else{
+            $(this).parents('.flex-form-soc').addClass('opened');
+        }
     });
 
     
